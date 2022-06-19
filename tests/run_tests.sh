4f773bb2532f5bc783b2f7ad93c888eb7d427abd
sytse <git@sreitsma.nl>
2022-06-19 20:06:33 +0200

    Regression and e2e tests can now also be executed on Linux

---
+++ tests/run_tests.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+if [ ! -e venv ]; then
+  echo Setting up venv
+  echo ========================================
+  python3 -m venv venv
+  venv/bin/pip install -r requirements.txt
+  echo
+fi
+
+export PATH=$(pwd)/venv/bin/:${PATH}
+export PYTHONPATH=$(pwd)/shared_modules
+
+echo Running regression tests
+echo ========================================
+cd regression_test
+nosetests -s -v .
+cd ..
+
+echo
+echo Running e2e tests
+echo ========================================
+# docker pull plantuml/plantuml-server
+
+DOCKER_ID=`sudo docker ps -qf "ancestor=plantuml/plantuml-server:jetty"`
+if [ "${DOCKER_ID}" = "" ]; then
+    echo Starting docker plantuml server
+    sudo docker run -it -d -p 8080:8080 plantuml/plantuml-server:jetty
+else
+    echo Docker plantuml server already running
+fi
+
+cd e2e_test
+nosetests -s -v .
+cd ..
