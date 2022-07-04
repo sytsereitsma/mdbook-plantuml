ae80bd57d25651e1dc853a9f4fc50610851bcc04
Sytse Reitsma <sreitsma@moog.com>
2022-07-04 21:35:04 +0200

    Display a more meaningful error when user configures http backend without server support

--- src/plantuml_backend_factory.rs
+++ src/plantuml_backend_factory.rs
@@ -3,8 +3,10 @@ use crate::plantuml_backend::PlantUMLBackend;
 use crate::plantuml_server_backend::PlantUMLServer;
 use crate::plantuml_shell_backend::PlantUMLShell;
 use crate::plantumlconfig::PlantUMLConfig;
+use anyhow::{bail, Result};
 #[cfg(any(feature = "plantuml-ssl-server", feature = "plantuml-server"))]
 use reqwest::Url;
+use std::path::Path;
 
 /// Create an instance of the PlantUMLBackend
 /// # Arguments
@@ -33,5 +35,23 @@ fn create_backend(cmd: &str) -> Box<dyn PlantUMLBackend> {
 
 #[cfg(not(any(feature = "plantuml-ssl-server", feature = "plantuml-server")))]
 fn create_backend(cmd: &str) -> Box<dyn PlantUMLBackend> {
-    Box::new(PlantUMLShell::new(cmd.to_string()))
+    if cmd.starts_with("http://") || cmd.starts_with("https://") {
+        Box::new(PlantUMLNoServerErrorBackend {})
+    } else {
+        Box::new(PlantUMLShell::new(cmd.to_string()))
+    }
+}
+
+struct PlantUMLNoServerErrorBackend;
+impl PlantUMLBackend for PlantUMLNoServerErrorBackend {
+    /// Display an error message when the user built the plugin without server
+    /// support, but does configure a server in book.toml.
+    fn render_from_string(&self, _: &str, _: &str, _: &Path) -> Result<()> {
+        bail!(
+            "A PlantUML server is configured, but the mdbook-plantuml plugin \
+            is built without server support.\nPlease rebuild/reinstall the \
+            plugin with server support, or configure the plantuml cli tool as \
+            backend. See the the Features section in README.md"
+        );
+    }
 }
